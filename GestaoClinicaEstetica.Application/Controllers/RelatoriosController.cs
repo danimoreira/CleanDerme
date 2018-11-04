da549d8ca9f56a9146b608e2e684de0bd5ed11ff
danimoreira <dmlurbana@gmail.com>
2018-11-04 11:34:56 -0200

    partial - inclusão relatórios

---
+++ GestaoClinicaEstetica.Application/Controllers/RelatoriosController.cs
@@ -0,0 +1,48 @@
+﻿using GestaoClinicaEstetica.Application.Controllers.Base;
+using GestaoClinicaEstetica.Domain.Interfaces.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GestaoClinicaEstetica.Application.Controllers
+{
+    public class RelatoriosController : BaseController
+    {
+        private readonly IClienteService _clienteService;
+
+        public RelatoriosController(IClienteService clienteService)
+        {
+            _clienteService = clienteService;
+        }
+
+        // GET: Relatorios/HistoricoCliente
+        public ActionResult HistoricoCliente()
+        {
+            UpdateBag();
+            return View();
+        }
+
+        public ActionResult FechamentoFinanceiro()
+        {
+            return View();
+        }
+
+        public ActionResult RepasseProfissional()
+        {
+            return View();
+        }
+
+        public override void UpdateBag()
+        {
+            base.UpdateBag();
+
+            ViewBag.ListaClientes = _clienteService.List().Select(x => new SelectListItem()
+            {
+                Value = x.Id.ToString(),
+                Text = x.Nome
+            });
+        }
+    }
+}
